07da2e8fc15d4801385f8386d3481bdf4964364b
Ashwin <ashwindeepak1997@gmail.com>
2024-07-09 12:54:00 +0530

    july9_error

---
+++ src/components/EditProject.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function EditProject() {
+  return (
+   <
+
+<Modal show={show} onHide={handleClose} size='lg'>
+    <Modal.Header closeButton>
+      <Modal.Title>Add Project Details</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Row>
+        <Col sm={12} md={6}>
+        <label for="">
+          <input type="file" style={{display:'none'}} />
+          <img src="https://static.thenounproject.com/png/2534766-200.png" alt="" width={'100%'}/>
+        </label>
+        </Col>
+
+        <Col sm={12} md={6}>
+        <form className='p-3' action="">
+          <div className='mb-3'>
+            <input type="text" placeholder='Title' className='form-control' />
+          </div>
+          <div className='mb-3'>
+          <input type="text" placeholder='Title' className='form-control' />
+          </div>
+          <div className='mb-3'>
+          <input type="text" placeholder='Title' className='form-control' />
+          </div>
+          <div className='mb-3'>
+          <input type="text" placeholder='Title' className='form-control' />
+          </div>
+          <div className='mb-3'></div>
+
+        </form>
+        </Col>
+      </Row>
+
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="warning" className='rounded-5' onClick={handleClose}>
+        Cancel
+      </Button>
+      <Button variant="success" className='rounded-5' onClick={handleClose}>
+        Add
+      </Button>
+    </Modal.Footer>
+  </Modal>
+   
+   </>
+  )
+}
+
+export default EditProject
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,11 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import { faPowerOff} from '@fortawesome/free-solid-svg-icons';
 
 
-function Header() {
+function Header({dash}) { /* dash is to send arg to dashboard for logout rendering */
   return (
     <>
       <Navbar expand="lg" className="bg-primary">
@@ -14,6 +16,11 @@ function Header() {
         <Link to='/' style={{textDecoration:'none'}}>
         <Navbar.Brand href="#home" className='text-light fs-4'> <FontAwesomeIcon icon={faStackOverflow} className=' fa-2x me-1'/> Projects</Navbar.Brand>
         </Link>
+        {/* Create a logout button to conditionally render */}
+        {
+          dash && <Button variant="warning" className='rounded-5 fs-5'><FontAwesomeIcon icon={faPowerOff} size='xl' className='me-2' />Logout</Button>
+        }
+        
       </Container>
     </Navbar>
     
