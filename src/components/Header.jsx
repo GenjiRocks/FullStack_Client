78768e16a23149021d6386977c7f1135a9eb1d93
Ashwin <ashwindeepak1997@gmail.com>
2024-08-01 09:43:50 +0530

    Logout_updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import { faPowerOff} from '@fortawesome/free-solid-svg-icons';
 
 
-function Header({dash}) { /* dash is to send arg to dashboard for logout rendering */
+function Header({dash}) {
+  const navigate = useNavigate() /* dash is to send arg to dashboard for logout rendering */
+  const handleLogout = () => {
+    // Clear session storage
+   
+    sessionStorage.removeItem("existingUser");
+    sessionStorage.removeItem("token"); 
+    navigate('/')
+  };
   return (
     <>
       <Navbar expand="lg" className="bg-primary">
@@ -18,7 +26,7 @@ function Header({dash}) { /* dash is to send arg to dashboard for logout renderi
         </Link>
         {/* Create a logout button to conditionally render */}
         {
-          dash && <Button variant="warning" className='rounded-5 fs-5'><FontAwesomeIcon icon={faPowerOff} size='xl' className='me-2' />Logout</Button>
+          dash && <Button variant="warning" onClick={handleLogout} className='rounded-5 fs-5'><FontAwesomeIcon icon={faPowerOff} size='xl' className='me-2' />Logout</Button>
         }
         
       </Container>
